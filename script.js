5054632c63ee04ddb4dd08ebfc5d3bfc2ba414cf
okumu87 <okumu793@gmail.com>
2026-09-06 22:15:39 +0200

    fixing bugs

--- script.js
+++ script.js
@@ -1,32 +1,49 @@
 const form = document.querySelector('form');
+const totalItems = [];
+
 form.addEventListener('submit', (e)=> {
     e.preventDefault();
+    let nextId = 0;
+    const currentId = nextId++;
     const expenseInput= document.getElementById('expense');
     const amountInput = document.getElementById('amount');
     const expenseName = expenseInput.value;
     const expenseAmount = amountInput.value;
    
     const itemList = document.createElement('li');
+    const spanItem = document.createElement('span');
     const deleteButton = document.createElement('button');
     deleteButton.classList.add('delete-btn');
     deleteButton.textContent = 'Delete';
-    itemList.textContent = `${expenseName}: $${expenseAmount}`;
+   
+    spanItem.textContent = `${expenseName}: $${expenseAmount}`;
+    itemList.appendChild(spanItem);
     itemList.appendChild(deleteButton);
+    itemList.dataset.id = currentId; // Assign a unique ID 
+    totalItems.push({ id: currentId, name: expenseName, amount: parseFloat(expenseAmount) });
     document.getElementById('expense-list').appendChild(itemList);
 
     //  expenseInput.value = '';
     //  amountInput.value = '';
 
     form.reset(); // Reset the form fields after submission
+
+    console.log(totalItems);
 })
 
 const expenseList = document.getElementById('expense-list');
+
 
 expenseList.addEventListener('click', (e) => {
     console.log(e.target);
 
     if (e.target.classList.contains('delete-btn')) {
         const listItem = e.target.parentElement;
+        const itemId = parseInt(listItem.dataset.id, 10);
+        totalItems.splice(totalItems.findIndex(item => item.id === itemId), 1);
         listItem.remove();
     }
+
+
 })
+
